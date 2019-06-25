64e935ff58cf93de35b0d271e268f7a2e1e9b198
Seth Lewis <lewiscatenary@gmail.com>
2019-06-25 16:28:54 -0500

    added card rotation via css

--- js/about.js
+++ js/about.js
@@ -1,13 +1,20 @@
 const cards = document.querySelectorAll('.flip-card');
 
 cards.forEach((card) => {
-  card.addEventListener('click', () => {
+  card.addEventListener('dblclick', () => {
     card.style.display = "none";
   })
 });
 
 cards.forEach((card) => {
   card.addEventListener('mouseover', () => {
-    card.title = "click to make this card vanish";
+    card.title = "double click to make this card vanish";
+    card.firstElementChild.style.transform = "rotateY(180deg)";
+  })
+});
+
+cards.forEach((card) => {
+  card.addEventListener('mouseleave', () => {
+    card.firstElementChild.style.transform = "rotateY(0deg)";
   })
 });
