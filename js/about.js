2055952a3be1bf8bae894ebbf61aa9f08193d56a
Seth Lewis <lewiscatenary@gmail.com>
2019-06-25 21:56:00 -0500

    netlify//gulp-less integration

--- js/about.js
+++ js/about.js
@@ -1,37 +1,13 @@
 const cards = document.querySelectorAll('.flip-card');
 
 cards.forEach((card) => {
-  card.addEventListener('dblclick', () => {
+  card.addEventListener('click', () => {
     card.style.display = "none";
   })
 });
 
 cards.forEach((card) => {
   card.addEventListener('mouseover', () => {
-    card.title = "double click to make this card vanish";
-    card.firstElementChild.style.transform = "rotateY(180deg)";
+    card.title = "click to make this card vanish";
   })
-});
-
-cards.forEach((card) => {
-  card.addEventListener('mouseleave', () => {
-    card.firstElementChild.style.transform = "rotateY(0deg)";
-  })
-});
-
-cards.forEach((card) => {
-  card.addEventListener('click', (e) => {
-    cards.style.opacity = "0.5";
-    e.target.style.opacity = "1";
-  })
-});
-
-// const teamMembersContainer = document.querySelector('.teamMembersContainer');
-cards.forEach((card) => {
-  const flipInner = card.firstElementChild;
-  flipInner.className = "flip-inner";
-  const flipcardFront = flipInner.firstElementChild;
-  flipcardFront.className = "flip-card-front";
-  flipcardBack = flipInner.children[1];
-  flipcardBack.className = "flip-card-back";
 });
