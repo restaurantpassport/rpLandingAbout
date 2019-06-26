93f1231526ca1ad28c9aff814936c60379b5989d
Seth Lewis <lewiscatenary@gmail.com>
2019-06-26 10:37:53 -0500

    changed card action to double click

--- js/about.js
+++ js/about.js
@@ -1,13 +1,13 @@
 const cards = document.querySelectorAll('.flip-card');
 
 cards.forEach((card) => {
-  card.addEventListener('click', () => {
+  card.addEventListener('dblclick', () => {
     card.style.display = "none";
   })
 });
 
 cards.forEach((card) => {
   card.addEventListener('mouseover', () => {
-    card.title = "click to make this card vanish";
+    card.title = "double click to make this card vanish";
   })
 });
