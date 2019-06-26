ef5a1bf5a81f730be35d833083d6d5cffcb9292c
Seth Lewis <lewiscatenary@gmail.com>
2019-06-26 17:43:39 -0500

    removed commented out code

--- js/about.js
+++ js/about.js
@@ -1,45 +1,8 @@
-// const cards = document.querySelectorAll('.flip-card');
-
-// cards.forEach((card) => {
-//   card.addEventListener('dblclick', () => {
-//     card.style.display = "none";
-//   })
-// });
-
-// cards.forEach((card) => {
-//   card.addEventListener('mouseover', () => {
-//     card.title = "double click to make this card vanish";
-//   })
-// });
-
-// cards.forEach((card) => {
-//   card.addEventListener('touchstart', () => {
-//     const cardRole = card.firstElementChild;
-//     cardRole.style.transform = "rotateY(180deg)";
-//   })
-// });
-
-
-// cards.forEach((card) => {
-//   card.addEventListener('mouseover', () => {
-//     const cardRole = card.firstElementChild;
-//     cardRole.style.transform = "rotateY(180deg)";
-//   })
-// });
-
-// cards.forEach((card) => {
-//   card.addEventListener('mouseout', () => {
-//     const cardRole = card.firstElementChild;
-//     cardRole.style.transform = "rotateY(0deg)";
-//   })
-// });
-
 class Cardtrick {
   constructor(flipcard) {
     this.flipcard = flipcard;
 
     this.flipFirstChild = flipcard.firstElementChild;
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.flipFirstChild.addEventListener('mouseover', () => {
       this.rotateover();
     });
